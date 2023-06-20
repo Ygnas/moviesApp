69c1840ac1eae728d1186c8047ce041788bb02b6
Ygnas <Ygnas@outlook.com>
2023-06-20 23:15:49 +0100

    Movie cast api

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -99,3 +99,12 @@ export const getTrendingMovies = () => {
       throw error
     });
 };
+
+export const getMediaCast = (id) => {
+  return fetch(
+    `https://api.themoviedb.org/3/movie/${id}/credits?language=en-US&api_key=${import.meta.env.VITE_TMDB_KEY}`
+  ).then((res) => res.json())
+    .then((json) => {
+      return json.cast;
+    });
+};
