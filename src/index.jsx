5d0095ad6189de78daa0bf7b75064ea106d56ea2
Ygnas <Ygnas@outlook.com>
2023-06-25 20:02:12 +0100

    New route for actor details page

--- src/index.jsx
+++ src/index.jsx
@@ -12,6 +12,7 @@ import { ReactQueryDevtools } from 'react-query/devtools'
 import MoviesContextProvider from "./contexts/moviesContext";
 import AddMovieReviewPage from './pages/addMovieReviewPage'
 import TrendingMoviesPage from "./pages/trendingMoviesPage";
+import ActorDetailsPage from "./pages/actorDetailsPage";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -30,6 +31,7 @@ const App = () => {
         <SiteHeader />
         <MoviesContextProvider>
           <Routes>
+            <Route path="/actor/:id" element={<ActorDetailsPage />} />
             <Route path="/movies/trending" element={<TrendingMoviesPage />} />
             <Route path="/reviews/form" element={<AddMovieReviewPage />} />
             <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
