93e829b43c38dcb5c84807124a7f1c6ff03827cf
Ygnas <Ygnas@outlook.com>
2023-07-29 12:02:37 +0100

    All media can be reordered by arrrow button

--- src/components/mediaList/index.jsx
+++ src/components/mediaList/index.jsx
@@ -1,18 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Movie from "../movieCard";
 import Grid from "@mui/material/Grid";
 import MediaActor from "../mediaActor";
 
 const MediaList = ({ media, action }) => {
-  let mediaCards = media.map((m) => (
-    <Grid key={m.id} item xs={12} sm={6} md={4} lg={3} xl={2}>
-      {m.title ?
-        <Movie key={m.id} movie={m} action={action} />
-        :
-        <MediaActor key={m.id} actor={m} action={action} />}
-    </Grid>
-  ));
-  return mediaCards;
+  const [ordered, setOrdered] = useState(media);
+
+  useEffect(() => {
+    setOrdered(media);
+  }, [media]);
+
+  const moveLeft = (id) => {
+    const index = ordered.findIndex((m) => m.id === id);
+    if (index > 0) {
+      const newOrderedMedia = [...ordered];
+      [newOrderedMedia[index - 1], newOrderedMedia[index]] = [newOrderedMedia[index], newOrderedMedia[index - 1]];
+      setOrdered(newOrderedMedia);
+    }
+  };
+
+  const moveRight = (id) => {
+    const index = ordered.findIndex((m) => m.id === id);
+    if (index < ordered.length - 1) {
+      const newOrderedMedia = [...ordered];
+      [newOrderedMedia[index], newOrderedMedia[index + 1]] = [newOrderedMedia[index + 1], newOrderedMedia[index]];
+      setOrdered(newOrderedMedia);
+    }
+  };
+
+  return (
+    <>
+      {ordered.map((m) => (
+        <Grid key={m.id} item xs={12} sm={6} md={4} lg={3} xl={2}>
+          {m.title ? <Movie movie={m} action={action} moveLeft={moveLeft} moveRight={moveRight} /> : <MediaActor actor={m} action={action} />}
+        </Grid>
+      ))}
+    </>
+  );
 };
 
 export default MediaList;
